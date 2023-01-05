764e770117e252438b5fe4d1f48b1a55730a9310
Dibyataru Chakraborty <64766478+Dib2001@users.noreply.github.com>
2023-01-05 22:48:11 +0530

    Update firebase-conf.jsx

--- src/Admin/pages/Firebase/firebase-conf.jsx
+++ src/Admin/pages/Firebase/firebase-conf.jsx
@@ -2,16 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBPY8Q7cv_Fbn_ynhtgljFMVBAxLdhgVXU",
-//   authDomain: "local-2022.firebaseapp.com",
-//   databaseURL: "https://local-2022-default-rtdb.asia-southeast1.firebasedatabase.app",
-//   projectId: "local-2022",
-//   storageBucket: "local-2022.appspot.com",
-//   messagingSenderId: "696611966846",
-//   appId: "1:696611966846:web:f6dc716338c0c1bf5d2d2f"
-// };
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_LOCAL_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_LOCAL_FIREBASE_AUTH_DOMAIN,
