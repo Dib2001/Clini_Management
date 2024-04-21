666763f0ed051c309568f73caaead2d0b1df0de4
Dibyataru <dibyataruchakraborty@gmail.com>
2024-04-21 16:56:52 +0530

    21042024

--- Backend/src/main/java/com/example/Backend/entity/Hospitals.java
+++ Backend/src/main/java/com/example/Backend/entity/Hospitals.java
@@ -2,12 +2,8 @@ package com.example.Backend.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-//@Getter
-//@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
--- Backend/src/main/java/com/example/Backend/entity/Patients.java
+++ Backend/src/main/java/com/example/Backend/entity/Patients.java
@@ -1,12 +1,8 @@
 package com.example.Backend.entity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-//@Getter
-//@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
